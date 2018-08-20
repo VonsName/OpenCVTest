958790078f2e0ac0370ada0cb764e702b34db102
fengjianglin <fengjianglin6817@dingtalk.com>
2018-08-20 17:25:04 +0800

    update

--- OpenCVTest/main.cpp
+++ OpenCVTest/main.cpp
@@ -18,9 +18,15 @@ int main(int argc,char *argv[])
 		return -1;
 	}
 
-	//namedWindow("opcv",CV_WINDOW_AUTOSIZE);
-	//显示图片
-	imshow("OPCV", src);
+	namedWindow("opcv",CV_WINDOW_AUTOSIZE);
+	//显示图片 第一个参数和上面的要一致
+	imshow("opcv", src);
+
+	namedWindow("output window", CV_WINDOW_AUTOSIZE);
+	Mat output;
+	//将一张RGB图像转为灰度图像
+	cvtColor(src, output, CV_BGR2GRAY);
+	imshow("output window", output);
 	waitKey(0);
 	return 0;
 }
